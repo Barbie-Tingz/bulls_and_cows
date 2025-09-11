3d8a104e3e5f46a19f7ecaf6dec542947d11b5f0
Barbie-Tingz <jillianhenry0823@gmail.com>
2025-09-11 09:17:01 -0400

    The display stage issue is not resolved; instead of showing the correct order of hangman stages, it is showing the last stage over and over again. Ther is also the rendering mask issue that I am currently trying to solve. Created a function to test but I have not tested as of yet.

--- src/bin/hangman_of_doom.rs
+++ src/bin/hangman_of_doom.rs
@@ -1,9 +1,11 @@
 // Hangman of Doom (Guess the Word)
 
+// there are no accented letters or emojis in this project
+
 use std::io; 
 
 fn display_stage(lives: usize) -> &'static str {
-    let display_stage = [
+    let stages = [
     // empty gallows; stage 0
    "
     +---+
@@ -75,7 +77,11 @@ fn display_stage(lives: usize) -> &'static str {
   =========",
 ];
 
-display_stage[lives as usize]
+// clamping so that code does not panic when it reaches a highet number than 6
+let max_index = stages.len() -1; // instead of having the it clamped it takes the length of the frames so it won't panic
+
+stages[lives as usize] // this returns the array element, that is why there is no semicolon
+// now you index with clamped not lives so that the code does not panic when it reach > 6 or < 0
 }
 
 
@@ -83,26 +89,36 @@ fn main() {
     
     println!("Welcome to Hangman of Doom!"); 
 
-    // check so code does not panic when a bigger number than 6 is passed. Test check ONLY!
-    for i in 0..=9 {
+/*     // check so code does not panic when a bigger number than 6 is passed. Test check ONLY!
+     for i in 0..=9 {
         println!("Stage number: {}", i); 
         println!("{}", display_stage(i)); 
     }
     println!("End of loop!"); 
-
+ */ 
     let secret_word = "orc"; 
-    let mut mask = secret_word.len(); // uses the length of the secret word, not hardcoded
+    let mut intial_mask = &secret_word.len(); // uses the length of the secret word, not hardcoded
+
+    // rendering mask 
+    fn render(intial_mask) {
+        let mut output_string = ""; 
+
+        for i in 0..(intial_mask.len()-1) {
+            if i != 0 {
+                " " + output_string; 
+            }
+            intial_mask[i] + output_string
+        }
+        output_string
+    }
+ 
 
     // this variable stores mutable user_input. 
-//    let mut guess_input = String::new();
-//    io::stdin() // standard input handle 
-//        .read_line(&mut guessed_letters) // read a line of input into the mutable String
-//        .expect("Failed to read word. Please try again."); // handle potential errors 
+   let mut guess_input = String::new();
+   io::stdin() // standard input handle 
+       .read_line(&mut guessed_letters) // read a line of input into the mutable String
+       .expect("Failed to read word. Please try again."); // handle potential errors 
     
-    // this variable is used as a container or vector to hold the previous guesses from the user 
-//    let mut guessed_letters: Vec<char> = guess_input.trim().chars().collect(); 
-    // "chars" iterates through each letter one at a time
-    //"collect" transforms an iterator into a collection; HashMap or Vec or FromIterator trait
 
 //    println!("You entered: {},", guessed_letters.trim()); 
 
