e174ee5d92fece741c6a0128d47adb3eca130512
Barbie-Tingz <jillianhenry0823@gmail.com>
2025-09-13 18:52:32 -0400

    Set up a new function called read_guessed that reads user input and stores there guesses in a vector. Also added the conditional aspect to stop the user from input too many letters. Still a work in progress.

--- src/bin/hangman_of_doom.rs
+++ src/bin/hangman_of_doom.rs
@@ -86,45 +86,58 @@ stages[max_index as usize] // this returns the array element, that is why there 
 
 
     // rendering mask from the secret word dynamically to and reveals the letters when the usre guesses them correctly 
-    fn render_mask(intial_mask: &[char]) -> String {
-        let mut output_string = String::new(); 
+fn render_mask(intial_mask: &[char]) -> String {
+    let mut output_string = String::new(); 
 
-        for i in 0..(intial_mask.len()) {
-            if i != 0 {
-                output_string.push(' '); // use push(' ') to add space 
-            }
-            let ch = intial_mask[i]; // this is used to push the current char from the mask
-            output_string.push(ch); 
+    for i in 0..(intial_mask.len()) {
+        if i != 0 {
+            output_string.push(' '); // use push(' ') to add space 
         }
-        output_string
+        let ch = intial_mask[i]; // this is used to push the current char from the mask
+        output_string.push(ch); 
     }
+    output_string
+}
+
+fn read_guess(guessed_letters: &Vec<char>) -> char {
+   // this variable stores mutable user_input. 
+   let mut guess_input = String::new();
+   io::stdin() // standard input handle 
+       .read_line(&mut guess_input) // read a line of input into the mutable String
+       .expect("Failed to read letter. Please try again."); // handle potential errors 
+
+    let letter_guessed = guess_input.trim().chars().is_alphabetic(false).to_ascii_lowercase(); 
+
+    if letter_guessed > 1 {
+    println!("Too many letters!"); 
+    } else {
+    println!("You guessed : {},", letter_guessed); 
+    }
+
+}
 
 
 fn main() {
     
     println!("Welcome to Hangman of Doom!"); 
 
-     // check so code does not panic when a bigger number than 6 is passed. Test check ONLY!
+/*   // check so code does not panic when a bigger number than 6 is passed. Test check ONLY!
      for i in 0..=9 {
         println!("Stage number: {}", i); 
         println!("{}", display_stage(i)); 
     }
     println!("End of loop!"); 
- 
+ */ 
 
-    let secret_word = "orc"; // test by changing the word
+    let secret_word = "goblin"; // test by changing the word
     // this dynamically uses the secret word provided to dynamically masks the word with the amount of letters from the secret_word bank
-    let mut intial_mask: Vec<char> = vec!['_'; secret_word.len()]; 
+    let intial_mask: Vec<char> = vec!['_'; secret_word.len()]; 
 
     println!("{}", render_mask(&intial_mask)); 
 
-/*     // this variable stores mutable user_input. 
-   let mut guess_input = String::new();
-   io::stdin() // standard input handle 
-       .read_line(&mut guessed_letters) // read a line of input into the mutable String
-       .expect("Failed to read word. Please try again."); // handle potential errors 
-    
- */
+    // this should return the letter from read guess
+    println!("{}", read_guess(&guessed_letters)); 
+
 //    println!("You entered: {},", guessed_letters.trim()); 
 
 
